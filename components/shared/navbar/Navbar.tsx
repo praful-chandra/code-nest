2f2ebe4335896303963d002e4b700618eaa16c20
Praful Chandra <chandra.s.praful@gmail.com>
2023-10-08 14:28:55 +0530

    Theme switcher functionality done

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -21,7 +22,7 @@ const Navbar = () => {
       </Link>
       Global search
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const themes = [
+  {
+    value: "light",
+    label: "Light",
+    icon: "/assets/icons/lightModeIcon.svg",
+  },
+  {
+    value: "dark",
+    label: "Dark",
+    icon: "/assets/icons/darkModeIcon.svg",
+  },
+  {
+    value: "system",
+    label: "System",
+    icon: "/assets/icons/systemModeIcon.svg",
+  },
+];
