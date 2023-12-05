c75f140ceedb316ffd1872357b78675cac7d4af8
Praful Chandra <chandra.s.praful@gmail.com>
2023-12-05 23:05:13 +0530

    created community page

---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Community</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Page;
